f84cedd44976be650118a5d49a6db812fc073fb0
yaokunpeng <yaokunpeng@100tal.com>
2020-05-19 16:36:45 +0800

    init(main) : 首页初始化

---
+++ src/main/index.js
@@ -0,0 +1,10 @@
+import './index.css'
+import "@src/common/css/anime.css"
+import './particle-animation.js'
+
+document.addEventListener('click', () => {
+    // import('@src/plugins/live2d')
+    import(/* webpackChunkName: "live2d/live2d-vendors" */ '@src/plugins/live2d')
+})
+// let ORZ = "这是首页"
+// console.log(ORZ)
