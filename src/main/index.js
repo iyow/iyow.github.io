4a5ef91c4df10055ccdf3cf4194a91530cdb3fe5
yaokunpeng <yaokunpeng@100tal.com>
2020-08-19 22:37:18 +0800

    fix(load) : load event once

--- src/main/index.js
+++ src/main/index.js
@@ -5,6 +5,6 @@ import './particle-animation.js'
 document.addEventListener('click', () => {
     // import('@src/plugins/live2d')
     import(/* webpackChunkName: "live2d/live2d-vendors" */ '@src/plugins/live2d')
-})
+}, { once: true })
 // let ORZ = "这是首页"
 // console.log(ORZ)
